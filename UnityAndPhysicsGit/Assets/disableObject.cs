5c05cade788251574130c821c40a6059e2b26e74
Andrew Cualing <ayc300@nyu.edu>
2016-02-23 14:07:47 -0500

    final working build- physics not working in build yet

---
+++ UnityAndPhysicsGit/Assets/disableObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class disableObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameObject.SetActive(false);
+        }
+	}
+}
