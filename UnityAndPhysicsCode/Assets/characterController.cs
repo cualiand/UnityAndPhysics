5ddee840faab7b3fdb79e9cbea007f34c9755c2a
Andrew Cualing <ayc300@nyu.edu>
2016-02-29 11:40:21 -0500

    added npc detection script

---
+++ UnityAndPhysicsCode/Assets/characterController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class characterController : MonoBehaviour {
+
+    CharacterController myController;
+    public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+        myController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal"); //simulating a virtual joystick from -1  to 1, 0 means no movement
+        float vertical = Input.GetAxis("Vertical");
+
+        //do movement now
+        Vector3 movement = transform.forward * speed * vertical * Time.deltaTime;
+        myController.Move(movement + Physics.gravity * Time.deltaTime);
+
+        //do turning now
+        transform.Rotate(0f, horizontal * 90f * Time.deltaTime, 0f);
+        //note this does physically impossible things, like stopping instantly
+        //also doesn't deal with other physics objects in the environment
+	}
+}
---
+++ UnityAndPhysicsCode/Assets/rigidbodyController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rigidbodyController : MonoBehaviour
+{
+    public float speed = 5f;
+    Rigidbody rBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 forwardForce = transform.forward * Time.deltaTime * speed * vertical;
+        rBody.AddForce(forwardForce);
+        //for physics forces, do it in fixedupdate
+
+        transform.Rotate(0f, horizontal * 90f * Time.deltaTime, 0f);
+    }
+}
